ab1084579e471f0fef4152d891cef682490eaf3e
kucheroff <kucheroff@tut.by>
2023-03-01 16:04:51 +0300

    добавил описание к задачам в комментариях

--- Sem_1/hw_1_4/Program.cs
+++ Sem_1/hw_1_4/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.Clear();
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Clear();
 Console.Write("Введите число N: ");
 int n = int.Parse(Console.ReadLine());
 
