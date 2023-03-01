697a2364e6b5143ff9f45064eb017c9556b2210d
kucheroff <kucheroff@tut.by>
2023-03-01 15:17:29 +0300

    hw_1_1 is ready

---
+++ Sem_1/hw_1_1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+if (a > b) 
+    Console.Write($"Первое число {a} больше второго числа {b}.");
+else
+    Console.Write($"Второе число {b} больше первого числа {a}.");
