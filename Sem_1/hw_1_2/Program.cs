fa0ccce44b07bbdc52b0d9cf3ecc83fcdd614960
kucheroff <kucheroff@tut.by>
2023-03-01 15:33:56 +0300

    hw_1_2 is ready

---
+++ Sem_1/hw_1_2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int c = int.Parse(Console.ReadLine());
+
+int max_num = a;
+
+if (b > max_num) max_num = b;
+if (c > max_num) max_num = c;
+
+Console.Write($"Из чисел {a}, {b}, {c} максимальное число {max_num}.");
