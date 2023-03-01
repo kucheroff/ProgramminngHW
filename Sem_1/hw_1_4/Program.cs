efd3d97443daac385115f837ed4852be38903aa6
kucheroff <kucheroff@tut.by>
2023-03-01 16:00:31 +0300

    hw_1_4 is ready

---
+++ Sem_1/hw_1_4/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.Write($"Четные числа от 1 до {n}: ");
+for (int i=1; i <= n; i++)
+    if (i % 2 == 0) Console.Write(i + " ");
