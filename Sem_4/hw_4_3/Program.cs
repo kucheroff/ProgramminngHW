edd1c81132298849a1e771d15aab07870ffeefd4
kucheroff <kucheroff@tut.by>
2023-03-07 14:57:32 +0300

    hw_4_3 is ready header added

---
+++ Sem_4/hw_4_3/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] fillArray()
+{
+    int[] result = new int[8];
+    for (int i = 0; i < 8; i++)
+    {   
+        Console.Write($"Введите элемент массива с индексом {i}: ");
+        result[i] = int.Parse(Console.ReadLine());
+    }
+
+    return result;
+}
+
+void printArray(int [] array)
+{   
+    Console.Write("[");
+    for (int i = 0; i < array.Length-1; i++) Console.Write($"{array[i]}, ");
+    Console.Write($"{array[array.Length-1]}]");
+}
+
+Console.Clear();
+printArray(fillArray());
+
+
