26499a301f1ced4faf2faa411d668aa5a5085a8e
kucheroff <kucheroff@tut.by>
2023-03-07 14:38:17 +0300

    hw_4_2 is ready

---
+++ Sem_4/hw_4_2/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int getSumm(int num)
+{
+    int summ = 0;
+    while (num != 0)
+    {
+        summ += num % 10;
+        num /= 10;
+    }
+
+    return summ;
+}
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.Write($"Сумма цифр в числе - {getSumm(n)}");
