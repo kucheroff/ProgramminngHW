69edc85e32cf5554bf47127af02b5b7b11eb0aa9
vyacheslav <kucheroff@tut.by>
2023-03-20 18:30:20 +0300

    hw 6 ready

---
+++ Sem_6/hw_6_1/Program.cs
@@ -0,0 +1,12 @@
+﻿int numPositive(int[] mass)
+{
+    int  result = 0;
+    for (int i=0; i< mass.Length; i++) if (mass[i] > 0) result += 1;
+    return result;
+
+}
+
+Console.Clear();
+Console.WriteLine("Введите числа массива через пробел:");
+int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+Console.Write("Количество чисел больше нуля " + numPositive(arr));
